21ca55db3c2dedfd7ba6d123e2cd683df48c3b2a
Simranbhandari1 <simranbhandari604@gmail.com>
2026-04-02 09:36:48 +0530

    changes

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -1,9 +1,21 @@
 const {GoogleGenAI} = require('@google/genai');
-
+const {z} = require('zod');
+const {zodJsonSchema} = require('zod-to-json-schema');
 const ai = new GoogleGenAI({
     apiKey: process.env.GOOGLE_GENAI_API_KEY,
 }); 
+const interviewReportSchema = z.object({
+    technicalQuestions: z.array(z.object({ 
+        question: z.string().description("The technical question can be asked in the interview"),
+        intention: z.string().description("The intention of interviewer behind asking the technical question"),
+        answer: z.string().description("How to answer this question , what points to cover ,what approach to take while answering this question")
 
+    })),
+    
+})
+ async function generateInterviewReport({resumeText, selfDescription ,jobDescription}){
+    
+ }
 
 async function invokeGeminiAi(){
     const response = await ai.models.generateContent({
